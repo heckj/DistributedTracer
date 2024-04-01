244e109350bd3821dc98b48e1f6855fdf4cf6963
Joe Heck <heckj@mac.com>
2024-03-31 18:53:51 -0700

    test tracer weekend experiment

--- Package.swift
+++ Package.swift
@@ -1,23 +1,51 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
 
 import PackageDescription
+let sharedSwiftSettings: [SwiftSetting] = [.enableExperimentalFeature("StrictConcurrency")]
 
 let package = Package(
     name: "DistributedTracer",
+    platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DistributedTracer",
             targets: ["DistributedTracer"]),
     ],
+    dependencies: [
+        // Tracing
+        .package(url: "https://github.com/slashmo/swift-otel", branch: "main"),
+        // this ^^ brings in a MASSIVE cascade of dependencies
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
+        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.0.0"),
+        // MARK: - OTLP
+
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.23.1"),
+
+        // MARK: - Plugins
+
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
+
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DistributedTracer"),
+            name: "DistributedTracer",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
+                .product(name: "OTel", package: "swift-otel"),
+                .product(name: "OTLPGRPC", package: "swift-otel"),
+            ],
+            swiftSettings: sharedSwiftSettings
+        ),
         .testTarget(
             name: "DistributedTracerTests",
-            dependencies: ["DistributedTracer"]),
+            dependencies: ["DistributedTracer"],
+            swiftSettings: sharedSwiftSettings
+        ),
     ]
 )
