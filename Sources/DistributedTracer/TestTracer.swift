cfb9349b3c03d26bc09cf073d703c9e4f39774f7
Joseph Heck <heckj@mac.com>
2024-04-02 10:46:06 -0700

    expose a function that sets up a tracer and "runs stuff" within that closure AFTER the tracer is enabled and operational (#1)
    * test fails, need to investigate (and learn) more to see if I can make something like this work
    * backing off, but making things a bit nicer and simpler

--- Sources/DistributedTracer/TestTracer.swift
+++ Sources/DistributedTracer/TestTracer.swift
@@ -10,76 +10,75 @@ public actor TestTracer {
     private var bootstrapped = false
     public static let shared = TestTracer()
 
-    public var tracer:
-        // (any Tracer)?
-        OTelTracer<OTelRandomIDGenerator<SystemRandomNumberGenerator>, OTelConstantSampler, OTelW3CPropagator, OTelBatchSpanProcessor<OTLPGRPCSpanExporter, ContinuousClock>, ContinuousClock>?
+    // exposing the tracer so that test shutdown methods can get ahold of it
+    // and invoke a flush of the traces before the test execution is summarily
+    // terminated.
+    public var tracer: (any Tracer)?
 
+    // hanging on to the handle for the detached task that "runs" the tracer, but not
+    // sure if we need to be able to cancel it or not... at least in a test scenario.
+    // Since you can only bootstrap instrumentation once, it seems you can't even manipulate
+    // or replace a tracer once it's set up (at least with how Distributed Tracing is set up
+    // today, so this is private and currently unused.
+    private var tracerRunHandle: Task<Void, any Error>?
+
+    private func createTracer(serviceName: String) -> OTelTracer<OTelRandomIDGenerator<SystemRandomNumberGenerator>, OTelConstantSampler, OTelW3CPropagator, OTelBatchSpanProcessor<OTLPGRPCSpanExporter, ContinuousClock>, ContinuousClock> {
+        let resource = OTelResource(attributes: ["service.name": "\(serviceName)"])
+        let environment = OTelEnvironment.detected()
+        // Here we create an OTel span exporter that sends spans via gRPC to an OTel collector.
+        let exporter = try! OTLPGRPCSpanExporter(configuration: .init(environment: environment))
+        // This exporter is passed to a batch span processor.
+
+        // The processor receives ended spans from the tracer, batches them up, and finally
+        // forwards them to the exporter.
+        let processor = OTelBatchSpanProcessor(exporter: exporter, configuration: .init(environment: environment))
+
+        let myTracer = OTelTracer(
+            idGenerator: OTelRandomIDGenerator(),
+            sampler: OTelConstantSampler(isOn: true),
+            propagator: OTelW3CPropagator(),
+            processor: processor,
+            environment: environment,
+            resource: resource
+        )
+
+        // Once we have a tracer, we bootstrap the instrumentation system to use it.
+        // You can only bootstrap an instrument ONCE per process (ref:
+        // https://github.com/apple/swift-distributed-tracing/blob/main/Sources/Instrumentation/InstrumentationSystem.swift#L38-L39
+        // So this has to hold for all bits using the tracer.
+        InstrumentationSystem.bootstrap(myTracer)
+
+        return myTracer
+    }
+
+    // original, hack it into place mechanism - just run something in a detached task in the background.
+    // yeah, kind of ugly
     public func bootstrap(serviceName: String) async {
         if !bootstrapped {
-            let environment = OTelEnvironment.detected()
-            let resourceDetection = OTelResourceDetection(detectors: [
-                OTelProcessResourceDetector(),
-                OTelEnvironmentResourceDetector(environment: environment),
-                .manual(OTelResource(attributes: ["service.name": "\(serviceName)"])),
-            ])
-            let resource = await resourceDetection.resource(environment: environment, logLevel: .trace)
-
-            /*
-             Bootstrap the logging system to use the OTel metadata provider.
-             This will automatically include trace and span IDs in log statements
-             from your app and its dependencies.
-             */
-            LoggingSystem.bootstrap({ label, _ in
-                var handler = StreamLogHandler.standardOutput(label: label)
-                // We set the lowest possible minimum log level to see all log statements.
-                handler.logLevel = .trace
-                return handler
-            }, metadataProvider: .otel)
-            let logger = Logger(label: "example")
-
-            /*
-             Here we create an OTel span exporter that sends spans via gRPC to an OTel collector.
-             */
-            let exporter = try! OTLPGRPCSpanExporter(configuration: .init(environment: environment))
-            /*
-             This exporter is passed to a batch span processor.
-             The processor receives ended spans from the tracer, batches them up, and finally forwards them to the exporter.
-             */
-            let processor = OTelBatchSpanProcessor(exporter: exporter, configuration: .init(environment: environment))
-            /*
-             We need to await tracer initialization since the tracer needs
-             some time to detect attributes about the resource being traced.
-             */
-            let myTracer = OTelTracer(
-                idGenerator: OTelRandomIDGenerator(),
-                sampler: OTelConstantSampler(isOn: true),
-                propagator: OTelW3CPropagator(),
-                processor: processor,
-                environment: environment,
-                resource: resource
-            )
-            /*
-             Once we have a tracer, we bootstrap the instrumentation system to use it.
-             This configures your application code and any of your dependencies to use the OTel tracer.
-             */
-            InstrumentationSystem.bootstrap(myTracer)
-
-            let serviceGroup = ServiceGroup(
-                services: [myTracer],
-                gracefulShutdownSignals: [.sigint],
-                logger: logger
-            )
+            let tracer = createTracer(serviceName: serviceName)
 
             // Set up a detached task to run this in the background indefinitely.
             // - no cancellation, just GO
-            Task {
-                try await serviceGroup.run()
+            tracerRunHandle = Task {
+                try await tracer.run()
             }
 
+            self.tracer = tracer
             bootstrapped = true
-            tracer = myTracer
         }
     }
 
-    private init() {}
+    private init() {
+        // Akin to the Instrumentation system, the Logging
+        // system can only be bootstrapped once per process. Since it's orthogonal
+        // setup, but used by Tracing, we set it up here in the default initializer.
+        LoggingSystem.bootstrap({ label, _ in
+            var handler = StreamLogHandler.standardOutput(label: label)
+            // We set the lowest possible minimum log level to see all log statements.
+            handler.logLevel = .trace
+            return handler
+        }, metadataProvider: .otel)
+        let logger = Logger(label: "TestTracer Setup")
+        logger.debug("Logging system initialized")
+    }
 }
--- Tests/DistributedTracerTests/DistributedTracerTests.swift
+++ Tests/DistributedTracerTests/DistributedTracerTests.swift
@@ -1,12 +1,43 @@
-@testable import DistributedTracer
+import DistributedTracer
+import Tracing
 import XCTest
 
 final class DistributedTracerTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    override func setUp() async throws {
+        await TestTracer.shared.bootstrap(serviceName: "TracerTests")
+    }
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    override func tearDown() async throws {
+        if let tracer = await TestTracer.shared.tracer {
+            tracer.forceFlush()
+            // Testing does NOT have a polite shutdown waiting for a flush to complete, so
+            // we explicitly give it some extra time here to flush out any spans remaining.
+            try await Task.sleep(for: .milliseconds(100))
+
+            // This would be a whole lot better at the tail end of the entire suite, or at least
+            // at the class tearDown, but `override class func tearDown` doesn't appear to have
+            // an async callback where we can do cleanup/flush work like this, so it's in
+            // each test execution. Sorry about that.
+        }
+    }
+
+    func testClosureInvokerTest() async throws {
+        try await withSpan("example span") { span in
+
+            try await Task.sleep(for: .milliseconds(50))
+            span.addEvent(SpanEvent("EVENT!"))
+            try await Task.sleep(for: .milliseconds(50))
+            span.addEvent(SpanEvent("SECOND EVENT!"))
+        }
+    }
+
+    func testClosureInvokerTestAgain() async throws {
+        try await withSpan("example span") { span in
+
+            try await Task.sleep(for: .milliseconds(50))
+            span.addEvent(SpanEvent("EVENT!"))
+            try await Task.sleep(for: .milliseconds(50))
+            span.addEvent(SpanEvent("SECOND EVENT!"))
+        }
     }
 }
